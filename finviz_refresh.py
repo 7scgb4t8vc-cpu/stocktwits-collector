ba63eceb418c4f557c9785bf8ffe252d12b60902
RyanF470 <7scgb4t8vc@privaterelay.appleid.com>
2026-07-21 12:04:07 -0400

    Update FINVIZ_URL to include 'ind_stocksonly'

--- finviz_refresh.py
+++ finviz_refresh.py
@@ -20,7 +20,7 @@ FINVIZ_COLUMNS = "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,
 
 FINVIZ_URL = (
     "https://elite.finviz.com/export?v=152"
-    "&f=geo_usa"
+    "&f=geo_usa,ind_stocksonly"
     "&ft=4&c={columns}&auth={token}"
 )
 
